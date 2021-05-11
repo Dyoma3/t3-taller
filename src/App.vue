2898bdfe9ef4427771cd515aa35e646e6c815cbe
Dyoma3 <dinko.f.yoma@gmail.com>
2021-05-11 17:51:28 -0400

    map animation

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,9 @@
     <v-main>
       <v-container class="mt-2">
       <div class="d-flex align-center flex-column">
-        <div id="mapContainer" class="map-container"/>
+        <transition name="map">
+          <div id="mapContainer" v-if="mapVisible" class="map-container"/>
+        </transition>
       </div>
       </v-container>
     </v-main>
@@ -38,23 +40,27 @@ export default {
   data: () => ({
     map: null,
     mapLoading: true,
+    mapVisible: false,
   }),
   mounted() {
-    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
-    this.map = new mapboxgl.Map({
-      container: 'mapContainer',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      zoom: -1,
-      center: [0, 0],
-    });
-    this.map.on('load', () => {
-      this.map.resize();
-      this.mapLoading = false;
-      this.map.fitBounds([
-        [180, 84],
-        [-180, -60]
-      ]);
-    });
+    this.mapVisible = true;
+    setTimeout(() => {
+      mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
+      this.map = new mapboxgl.Map({
+        container: 'mapContainer',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: -1,
+        center: [0, 0],
+      });
+      this.map.on('load', () => {
+        this.map.resize();
+        this.mapLoading = false;
+        this.map.fitBounds([
+          [180, 84],
+          [-180, -60]
+        ]);
+      });
+    }, 200);
   },
 };
 </script>
@@ -63,5 +69,38 @@ export default {
   height: 500px;
   width:750px;
   overflow: hidden;
+}
+.bounce-enter-active {
+  animation: bounce 0.9s;
+}
+.bounce-leave-active {
+  animation: bounce 0.3s reverse;
+}
+@keyframes bounce {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.2);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+.map-enter-active {
+  transition: all 0.4s ease 1.2s;
+}
+.map-leave-active {
+  transition: all 0.3s;
+}
+.map-enter,
+.map-leave-to {
+  top: -100vh;
+  position: relative;
+}
+.map-enter-to,
+.map-leave {
+  top: 0vh;
+  position: relative;
 }
 </style>
